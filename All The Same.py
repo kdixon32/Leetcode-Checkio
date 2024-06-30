d0a59d2a9df524c7f4b58141f3ef2562c720872f
kdixon32 <kielandixon@gmail.com>
2024-06-30 18:04:03 -0400

    All the Same

--- All The Same.py
+++ All The Same.py
@@ -1,6 +1,7 @@
 #In this mission you should check if all elements in the given sequence are equal.
 from typing import List, Any
 def all_the_same(elements: List[Any]) -> bool:
-    if not elements:
+    #Check if list is populated
+    if not elements: 
         return True
     return len(set(elements)) == 1
